e2ae3ef5f98c8b90bf7193d899d30080f172f8e1
leihao <53589602+iel-h@users.noreply.github.com>
2022-04-28 16:56:25 +0800

    feat: 添加部署脚本 (#68)

--- packages/axios-ext-example/vite.config.ts
+++ packages/axios-ext-example/vite.config.ts
@@ -3,10 +3,5 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  server: {
-    proxy: {
-      '/api': 'https://www.fastmock.site/mock/9f00454804589fac6a7f595c559d2ab8'
-    }
-  },
   plugins: [vue()]
 })
