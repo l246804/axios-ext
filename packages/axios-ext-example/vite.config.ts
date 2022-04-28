8530f3598f933167ca21d7463a3631025bc59f82
leihao <2468048176@qq.com>
2022-04-28 18:26:19 +0800

    fix: 修复部署脚本

--- packages/axios-ext-example/vite.config.ts
+++ packages/axios-ext-example/vite.config.ts
@@ -1,7 +1,10 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
+const BASE_PATH = process.env.NODE_ENV === 'production' ? './' : '/'
+
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: BASE_PATH,
   plugins: [vue()]
 })
